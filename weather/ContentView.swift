af9b69b20c4ff57b9da3b48ddef3503ddc86b84a
caio malvezzi <caio.malvezzi@compasso.com.br>
2024-05-22 16:09:56 -0300

    Entendendo o SF Simbols

--- weather/ContentView.swift
+++ weather/ContentView.swift
@@ -14,6 +14,26 @@ struct ContentView: View {
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("Mogi Mirim, SP")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                VStack(spacing: 10) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    Text("76°")
+                        .font(.system(size: 70,
+                                      weight: .medium, 
+                                      design: .default))
+                        .foregroundStyle(.white)
+                }
+                Spacer()
+            }
         }
     }
 }
