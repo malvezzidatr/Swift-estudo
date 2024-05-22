43e98c0f57f4e79c5618519be3a8f4732dd84a90
caio malvezzi <caio.malvezzi@compasso.com.br>
2024-05-22 15:24:12 -0300

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Caio Malvezzi on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
