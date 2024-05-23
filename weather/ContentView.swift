b629ae350cdae25d453fbf0d35fd8ea9447ebcd3
Caio Malvezzi <malvezzi.dev@gmail.com>
2024-05-23 09:58:52 -0300

    Criacao de cor e variaveis

--- weather/ContentView.swift
+++ weather/ContentView.swift
@@ -10,28 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(colors: [.blue, .white],
+            LinearGradient(colors: [.blue, Color("lightBlue")],
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             VStack {
-                Text("Mogi Mirim, SP")
-                    .font(.system(size: 32, weight: .medium, design: .default))
-                    .foregroundColor(.white)
-                    .padding()
+                CityNameView(cityName: "Mogi Mirim", stateName: "SP")
+                TemperatureTodayView()
+                HStack() {
+                    DaysView(day: "TUE", temperature: "74°", iconName: "cloud.sun.fill")
+                    DaysView(day: "WED", temperature: "70°", iconName: "sun.max.fill")
+                    DaysView(day: "THU", temperature: "66°", iconName: "wind")
+                    DaysView(day: "FRI", temperature: "60°", iconName: "sunset.fill")
+                    DaysView(day: "SAT", temperature: "55°", iconName: "moon.stars.fill")
+                }.padding(.top, 20)
                 
-                VStack(spacing: 10) {
-                    Image(systemName: "cloud.sun.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 180, height: 180)
-                    Text("76°")
-                        .font(.system(size: 70,
-                                      weight: .medium, 
-                                      design: .default))
-                        .foregroundStyle(.white)
-                }
                 Spacer()
             }
         }
@@ -40,4 +33,62 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct CityNameView: View {
+    var cityName: String
+    var stateName: String
+    
+    var body: some View {
+        HStack() {
+            Text(cityName)
+                .font(.system(size: 36, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Text("-")
+                .font(.system(size: 36, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Text(stateName)
+                .font(.system(size: 36, weight: .medium, design: .default))
+                .foregroundColor(.white)
+        }.padding(.top, 25)
+    }
+}
+
+struct DaysView: View {
+    var day: String
+    var temperature: String
+    var iconName: String
+    
+    var body: some View {
+            VStack(spacing: 18) {
+                Text(day)
+                    .font(.system(size: 18, weight: .medium, design: .default))
+                    .foregroundStyle(.white)
+                Image(systemName: iconName)
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 55, height: 55)
+                Text(temperature)
+                    .font(.system(size: 24, weight: .medium, design: .default))
+                    .foregroundStyle(.white)
+            }.padding(.horizontal, 7)
+    }
+}
+
+struct TemperatureTodayView: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            Image(systemName: "cloud.sun.fill")
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            Text("76°")
+                .font(.system(size: 70,
+                              weight: .medium,
+                              design: .default))
+                .foregroundStyle(.white)
+        }
+    }
 }
